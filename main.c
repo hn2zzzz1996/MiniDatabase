d02e55c73f6a22dbfa1ce9fbbc7354d6500dc2f5
hn2zzzz1996 <huanggua5678@qq.com>
2019-09-29 13:07:31 +0800

    commit

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ typedef enum {
 
 typedef enum {
 	PREPARE_SUCCESS,
+	PREPARE_SYNTAX_ERROR,
 	PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -82,6 +83,7 @@ void close_input_buffer(InputBuffer **input_buffer) {
 	}
 }
 
+
 void print_prompt() {
 	printf("db > ");
 }
@@ -119,10 +121,58 @@ void free_table(Table **table) {
 	}
 }
 
+void print_row(Row *row){
+	printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
 
+void serialize_row(Row *row, void *dest){
+	memcpy(dest+ID_OFFSET, &row->id, ID_SIZE);
+	memcpy(dest+USERNAME_OFFSET, &row->username, USERNAME_SIZE);
+	memcpy(dest+EMAIL_OFFSET, &row->email, EMAIL_SIZE);
+}
 
-MetaCommandResult do_meta_command(InputBuffer *input_buffer){
+void deserialize_row(void *src, Row *row){
+	memcpy(&row->id, src+ID_OFFSET, ID_SIZE);
+	memcpy(&row->username, src+USERNAME_OFFSET, USERNAME_SIZE);
+	memcpy(&row->email, src+EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void *row_slot(Table *table, uint32_t row_num){
+	uint32_t page_num = row_num / ROWS_PER_PAGE;
+	void *page = table->pages[page_num];
+	if(page == NULL){
+		page = table->pages[page_num] = malloc(PAGE_SIZE);
+	}
+	uint32_t row_offset = row_num % ROWS_PER_PAGE;
+	uint32_t byte_offset = row_offset * ROW_SIZE;
+	return page + byte_offset;
+}
+
+ExecuteResult execute_insert(Table *table, Statement *statement){
+	if(table->num_rows >= TABLE_MAX_ROWS){
+		return EXECUTE_TABLE_FULL;
+	}
+
+	Row *row_to_insert = &statement->row_to_insert;
+	
+	serialize_row(row_to_insert, row_slot(table, table->num_rows));
+	table->num_rows += 1;
+
+	return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Table *table, Statement *statement){
+	Row row;
+	for(uint32_t i = 0; i < table->num_rows; i++){
+		deserialize_row(row_slot(table, i), &row);
+		print_row(&row);
+	}
+	return EXECUTE_SUCCESS;
+}
+
+MetaCommandResult do_meta_command(Table *table, InputBuffer *input_buffer){
 	if(!strcmp(input_buffer->buf, ".exit")){
+		free_table(&table);
 		close_input_buffer(&input_buffer);
 		printf("Bye.\n");
 		exit(EXIT_SUCCESS);
@@ -131,9 +181,16 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer){
 	}
 }
 
-PrepareResult prepare_command(InputBuffer *input_buffer, Statement *statement){
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement){
 	if(!strncmp(input_buffer->buf, "insert", 6)){
 		statement->type = STATEMENT_INSERT;
+		int args_assigned = sscanf(input_buffer->buf, 
+				"insert %d %s %s", &statement->row_to_insert.id, 
+				statement->row_to_insert.username,
+				statement->row_to_insert.email);
+		if(args_assigned < 3){
+			return PREPARE_SYNTAX_ERROR;
+		}
 		return PREPARE_SUCCESS;
 	} 
 	if(!strncmp(input_buffer->buf, "select", 6)){
@@ -144,18 +201,17 @@ PrepareResult prepare_command(InputBuffer *input_buffer, Statement *statement){
 	return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-void execute_statement(Statement *statement){
+ExecuteResult execute_statement(Table *table, Statement *statement){
 	switch(statement->type){
 		case STATEMENT_INSERT:
-			printf("Do an insert.\n");
-			break;
+			return execute_insert(table, statement);
 		case STATEMENT_SELECT:
-			printf("Do an select.\n");
-			break;
+			return execute_select(table, statement);
 	}
 }
 
 int main(int argc, char *argv[]) {
+	Table *table = new_table();
 	InputBuffer *input_buffer = new_input_buffer();
 
 	while(1) {
@@ -163,7 +219,7 @@ int main(int argc, char *argv[]) {
 		read_input(input_buffer);
 
 		if(input_buffer->buf[0] == '.'){
-			switch(do_meta_command(input_buffer)){
+			switch(do_meta_command(table, input_buffer)){
 				case META_COMMAND_SUCCESS:
 					continue;
 				case META_COMMAND_UNRECOGNIZED_COMMAND:
@@ -173,15 +229,25 @@ int main(int argc, char *argv[]) {
 		}
 
 		Statement statement;
-		switch(prepare_command(input_buffer, &statement)){
+		switch(prepare_statement(input_buffer, &statement)){
 			case PREPARE_SUCCESS:
 				break;
+			case PREPARE_SYNTAX_ERROR:
+				printf("Syntax error. Could not parse statement.\n");
+				continue;
 			case PREPARE_UNRECOGNIZED_STATEMENT:
 				printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buf);
 				continue;
 		}
 
-		execute_statement(&statement);
-		printf("Executed Success.\n");
+		switch(execute_statement(table, &statement)){
+			case EXECUTE_SUCCESS:
+				printf("Executed\n");
+				break;
+			case EXECUTE_TABLE_FULL:
+				printf("Error: Table full\n");
+				break;
+		}
 	}
+
 }
